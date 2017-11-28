7f5b2c62272858d853f9c7ee16a06c76aa5b9430
Timo Michna <timomichna@yahoo.de>
2017-11-28 08:11:00 +0100

    feat: add brew integration test suite

---
+++ tests/integration/brew/suite.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# bootstrap
+DIR="$( cd "$( dirname $0)" && pwd )";
+THIS="${DIR}/${0##*/}";
+ROOT_DIR="$( cd "$( dirname "$0")/../../.." && pwd )";
+SHUNIT="${ROOT_DIR}/vendor/shunit2/shunit2";
+HELPER_DIR="${ROOT_DIR}/helpers";
+SHUNIT="${ROOT_DIR}/vendor/shunit2/shunit2";
+
+# include test files
+for F in ${DIR}/*
+do
+    if [ $F != $THIS ]; then
+        if [ ! -d $F ]; then
+            . ${F}
+        fi
+    fi
+done
+
+# suite
+function suite {
+    # detect-pkg-manager
+    suite_addTest testDetectMngrBrew
+}
+
+
+
+echo "*****************************************"
+echo "* RUNNING BREW TEST SUITE";
+echo "*****************************************"
+
+. ${SHUNIT}
