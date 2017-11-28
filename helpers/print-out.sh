73c9e2d15e4e6f516df22b645e35d59cf7e61701
Timo Michna <timomichna@yahoo.de>
2017-11-28 07:58:07 +0100

    feat: add print helper

---
+++ helpers/print-out.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# print function
+function printLine {
+    echo "> $1";
+}
+
+function printMessage {
+    echo "*****************************************************"
+    echo "* $1";
+    echo "*****************************************************"
+}
+
