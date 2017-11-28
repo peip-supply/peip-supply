2e3c0a5b3421aa0c48136398d0aa7c48a58d69dc
root <root@vps441293.ovh.net>
2017-11-28 11:18:27 +0100

    feat: add shunit2 package

---
+++ scripts/shunit2/bash/install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ ! $(command -v "shunit2") ]; then
+
+    cd ~
+    mkdir shunit2
+    cd shunit2
+    curl -o shunit2.zip https://codeload.github.com/kward/shunit2/zip/master
+    unzip -q ./shunit2.zip
+    mv ./shunit2-master /usr/local/lib/shunit2
+    ln -s /usr/local/lib/shunit2/shunit2 /usr/local/bin/shunit2
+    cd ~ 
+    rm -f -r ./shunit2
+
+fi
