433f1fd8ddd018c0b6727c2009f57a318f2e6244
Timo Michna <timomichna@yahoo.de>
2017-11-28 04:02:41 +0100

    feat: change test to look for default package

--- tests/unit/helpers/find-pkg-dir-test.sh
+++ tests/unit/helpers/find-pkg-dir-test.sh
@@ -5,7 +5,7 @@ TESTED="${HELPER_DIR}/find-pkg-dir.sh"
 source ${TESTED}
 
 detectPkgDir_testNotNull() {
-    DIR=$(findPackageDir "apt" "curl")
+    DIR=$(findPackageDir "bash" "peip")
     assertEquals "exit code should be 0" 0 $?
     assertNotNull "Result should be not NULL" ${DIR}
 }
