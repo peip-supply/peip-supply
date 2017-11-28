beebd8733d8fe44a5dd9950d6ac7f348087eaadf
Timo Michna <timomichna@yahoo.de>
2017-11-28 06:55:28 +0100

    feat: add get-native-installer

---
+++ helpers/get-native-installer.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
+source "${DIR}/get-root-dir.sh"
+
+function getNativeInstaller {
+    # parameters package-manager and package-name are needed
+    if [ ! $# == 2 ]; then
+      echo "Usage: $0 package-manager package-name"
+      exit
+    fi
+    MNGR=$1
+    PKG=$2
+    ROOT_DIR=$(getRootDir);
+    MANAGER_DIR="${ROOT_DIR}/managers/${MNGR}"
+    expr "$(${MANAGER_DIR}/install.sh ${PKG})"
+}
