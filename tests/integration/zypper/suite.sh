7d40d83badd1ac765c9ac720a324a942d2d65448
root <root@vps441293.ovh.net>
2017-11-29 04:37:54 +0100

    feat: add zypper integration test suite

---
+++ tests/integration/zypper/suite.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# bootstrap
+DIR="$( cd "$( dirname $0)" && pwd )";
+THIS="${DIR}/${0##*/}";
+ROOT_DIR="$( cd "$( dirname "$0")/../../.." && pwd )";
+SHUNIT="${ROOT_DIR}/vendor/shunit2/shunit2";
+HELPER_DIR="${ROOT_DIR}/helpers";
+SHUNIT="${ROOT_DIR}/vendor/shunit2/shunit2";
+
+# include test files
+for F in ${DIR}/*
+do
+    if [ $F != $THIS ]; then
+        if [ ! -d $F ]; then
+            . ${F}
+        fi
+    fi
+done
+
+# suite
+function suite {
+    # detect-pkg-manager
+    suite_addTest testDetectMngrZypper
+}
+
+
+
+echo "*****************************************"
+echo "* RUNNING ZYPPER TEST SUITE";
+echo "*****************************************"
+
+if [ $(command -v "shunit2") ]; then
+    source shunit2
+else
+    . ${SHUNIT}
+fi
